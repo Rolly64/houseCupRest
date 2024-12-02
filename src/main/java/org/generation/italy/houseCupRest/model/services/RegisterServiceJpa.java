eabae7b65fffe2377f705f883ee10fd0dd0b38b0
Andrea Macellaro La Franca <andrea.macellaro@gmail.com>
2024-12-02 09:29:40 +0100

    c

--- src/main/java/org/generation/italy/houseCupRest/model/services/RegisterServiceJpa.java
+++ src/main/java/org/generation/italy/houseCupRest/model/services/RegisterServiceJpa.java
@@ -50,7 +50,7 @@ public class RegisterServiceJpa implements RegisterService{
     }
 
     @Override
-    public List<Teacher> findAllTeachers() { // aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaah
+    public List<Teacher> findAllTeachers() {
         return teacherRepo.findAll();
     }
 
